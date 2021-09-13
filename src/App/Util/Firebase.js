5993e1d0ec5a72eefdf15dd1e8c97915eea78f5d
David Jegat <david.jegat@gmail.com>
2021-09-13 15:31:25 +0200

    Ajout de la configuration firebase

--- src/App/Util/Firebase.js
+++ src/App/Util/Firebase.js
@@ -3,12 +3,12 @@ import 'firebase/auth'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDpoX9GrV6RzQTVrLRoSksoDJVIQumUoT4',
-  authDomain: 'cookthat-8cd9b.firebaseapp.com',
-  projectId: 'cookthat-8cd9b',
-  storageBucket: 'cookthat-8cd9b.appspot.com',
-  messagingSenderId: '263852106888',
-  appId: '1:263852106888:web:7f556f3cea2895cf3e6b3d',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEI,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 }
 
 export const app = firebase.initializeApp(firebaseConfig)
